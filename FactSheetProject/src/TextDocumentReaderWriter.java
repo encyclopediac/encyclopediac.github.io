2444da043f1e7f27ffa05d762409537b3059f683
encyclopediac <75314605+encyclopediac@users.noreply.github.com>
2021-03-24 17:17:10 -0300

    Presentation improvements
    Now that I understand CSS and HTML better, I improved the layout of the TextFileExperiment.html document. This includes adding a 'center.main' (see CSS) and adding colours to a foreground and background. A customised font was chosen (Helvetica) and google fonts API was abandoned due to privacy concerns.  Finally, the source titles (h4) were made not bold and italic, to distinguish them better.

--- FactSheetProject/src/TextDocumentReaderWriter.java
+++ FactSheetProject/src/TextDocumentReaderWriter.java
@@ -13,17 +13,23 @@ public class TextDocumentReaderWriter {
     private final static String H3_PATTERN = "### ";
     private final static String ONELEVELDOWN_PATTERN = "##o";
     private final static String ONELEVELUP_PATTERN = "##c";
+    private final static String TITLE_META = "<meta property=\"og:title\" content=";
+    private final static String HEAD_HTML_1 = "<html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width\"><meta charset=\"utf-8\">";
+    private final static String HEAD_HTML_2 = "<meta charset=\"utf-8\"/><link rel=\"stylesheet\" href=\"CSSFile.css\"></head>";
+    private final static String CENTER_MAIN = "<div id=\"main\" class=\"center main\">";
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new FileReader(READ_FILEPATH));
         File writeToHTML = new File(WRITE_FILEPATH);
         writeToHTML.delete();
+        
         BufferedWriter writer = new BufferedWriter(new FileWriter(WRITE_FILEPATH));
         String currentLine = reader.readLine();
         while (currentLine != null) {
             oneLevelDeeper(reader, writer, currentLine);
             currentLine = reader.readLine();
         }
+        writer.write("</div>");
         reader.close();
         writer.close();
     }
@@ -35,7 +41,11 @@ public class TextDocumentReaderWriter {
         } else if (currentLine.contains(ONELEVELDOWN_PATTERN)) {
             oneLevelDeeper(reader, writer, currentLine);
         } else if (currentLine.contains(TITLE_PATTERN)) {
+            writer.write(TITLE_META + "\""+ currentLine.split(TITLE_PATTERN)[1] + "\">");
+            writer.write(HEAD_HTML_1);
             writer.write("<title>" + currentLine.split(TITLE_PATTERN)[1] + "</title>");
+            writer.write(HEAD_HTML_2);
+            writer.write(CENTER_MAIN);
             writer.newLine();
         } else if (currentLine.contains(H1_PATTERN)) {
             writer.write("<h1>" + currentLine.split(H1_PATTERN)[1] + "</h1>");
@@ -58,7 +68,7 @@ public class TextDocumentReaderWriter {
         writer.write("<body>");
         while (!(currentLine.contains(ONELEVELUP_PATTERN))) {
             if (currentLine.charAt(0) == '-') {
-                writer.write("<u1><li>" + currentLine.split("-")[1] + "</li></u1>");
+                writer.write("<u1><li>" + currentLine.split("-")[1] + "</li></u1> <p></p>");
                 writer.newLine();
             } else {
                 writer.write("<h4>" + currentLine + "</h4>");
